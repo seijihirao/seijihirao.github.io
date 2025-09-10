d97d3953854c622a3cbab5a06d79826ce56a814f
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2025-09-10 15:31:58 -0300

    Refactor: wedding envelope.

--- public/wedding/scripts/alpine-components.js
+++ public/wedding/scripts/alpine-components.js
@@ -56,4 +56,48 @@ document.addEventListener('alpine:init', () => {
             rotation: getRandomRotation()
         }))
     }));
+
+    // Countdown timer component
+    Alpine.data('countdownTimer', () => ({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        targetDate: new Date('2026-12-05T00:00:00').getTime(),
+        interval: null,
+
+        startCountdown() {
+            this.updateCountdown();
+            this.interval = setInterval(() => {
+                this.updateCountdown();
+            }, 1000);
+        },
+
+        updateCountdown() {
+            const now = new Date().getTime();
+            const distance = this.targetDate - now;
+
+            if (distance < 0) {
+                this.days = 0;
+                this.hours = 0;
+                this.minutes = 0;
+                this.seconds = 0;
+                if (this.interval) {
+                    clearInterval(this.interval);
+                }
+                return;
+            }
+
+            this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        },
+
+        destroy() {
+            if (this.interval) {
+                clearInterval(this.interval);
+            }
+        }
+    }));
 }); 
